1b8e420e1c3e742d58ba9b0255cbc855e418eea5
sKabYY <skabyy@gmail.com>
2013-08-04 15:34:17 +0800

    Add member function module to LuaState

---
+++ test/base_wrapper.h
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "../luacapi/lua.hpp"
+#include "../luabacc/luabacc.h"
+#include "../luabacc/dump.h"
+using namespace luabacc;
+
+
+inline bool exp_with_msg(bool exp, std::string msg) {
+	if (!exp) {
+		std::cout << msg << std::endl;
+	}
+	return exp;
+}
+
+
+void func(LuaState& state);
+
+
+inline bool flt_equals(double a, double b) {
+	const double ETA = 0.0001;
+	return b < a + ETA && b > a - ETA;
+}
+
+
+inline std::string getStackEmptyMsg() {
+	static std::string EMPTY_LUA_STACK_MSG = "top=0:\n";
+	return EMPTY_LUA_STACK_MSG;
+}
+
+
+int main() {
+	LuaState state;
+	try {
+		func(state);
+	} catch (std::string e) {
+		std::cerr << e << std::endl;
+	}
+	std::string dump_msg = dumpLuaState(state.state());
+	// Check stack
+	assert(exp_with_msg(dump_msg == getStackEmptyMsg(), dump_msg));
+	return 0;
+}
